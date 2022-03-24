ad45c27ea66ff48c8cdec60bd000b9c0f34459b0
Diana Putnam <dianaa.putnam@gmail.com>
2022-03-24 14:12:51 -0600

    navlist and img

--- starships/sw-starships.js
+++ starships/sw-starships.js
@@ -1,3 +1,33 @@
 import { starships } from "../data/starships.js";
+import { removeChildren, getLastNumber } from "../utils/index.js";
 
-console.log(starships)
+const nav = document.querySelector('.nav')
+const navList = document.querySelector('.navList')
+const shipViewer = document.querySelector('.shipViewer')
+
+function populateNav() {
+    starships.forEach((starship) => {
+        const listItem = document.createElement('li')
+        const anchor = document.createElement('a')
+        anchor.href = '#'
+        anchor.textContent = starship.name
+        anchor.addEventListener('click', () => populateShipView(starship))
+
+        listItem.appendChild(anchor)
+        navList.appendChild(listItem)
+    })
+}
+
+populateNav()
+
+function populateShipView(shipData) {
+    removeChildren(shipViewer)
+
+    const shipImage = document.createElement('img')
+    let shipNum = getLastNumber(shipData.url)
+    shipImage.src = `https://starwars-visualguide.com/assets/img/starships/${shipNum}.jpg`
+
+    shipViewer.appendChild(shipImage)
+    
+
+}
