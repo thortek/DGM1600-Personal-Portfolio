d5d97b3c5b4df2092a09334f97622133a1414d1d
Diana Putnam <dianaa.putnam@gmail.com>
2022-03-24 21:00:07 -0600

    modalbox

--- starships/sw-starships.js
+++ starships/sw-starships.js
@@ -5,6 +5,13 @@ const nav = document.querySelector('.nav')
 const navList = document.querySelector('.navList')
 const shipViewer = document.querySelector('.shipViewer')
 
+const modal = document.querySelector('.modal')
+const closeButton = document.querySelector('.modal-close')
+// TODO: add a click event listener for the modal-background div as well
+const shipMessage = document.querySelector('.box p')
+
+closeButton.addEventListener('click', () => modal.classList.toggle('is-active'))
+
 function populateNav() {
     starships.forEach((starship) => {
         const listItem = document.createElement('li')
@@ -26,6 +33,14 @@ function populateShipView(shipData) {
     const shipImage = document.createElement('img')
     let shipNum = getLastNumber(shipData.url)
     shipImage.src = `https://starwars-visualguide.com/assets/img/starships/${shipNum}.jpg`
+    shipImage.addEventListener('error', () => {
+        console.log("Image error!!!!!!")
+        shipImage.hidden = true
+        shipMessage.textContent = `The ship known as ${shipData.name} is currently in space port for repairs.`
+        modal.classList.toggle('is-active')
+    })
+
+
 
     shipViewer.appendChild(shipImage)
     
